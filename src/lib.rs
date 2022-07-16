3a507f7f66be5420e0936da758c21d813542aebb
TomascpMarques <tomascpmarques@gmail.com>
2022-07-16 14:40:59 +0100

    Using lib as a module conecter for the package

--- src/lib.rs
+++ src/lib.rs
@@ -1,25 +1,6 @@
-pub use clap::Parser;
-
-/// A CLI tool to research your programming doubts
-#[derive(Parser, Debug)]
-#[clap(author, version, about, long_about = None)]
-pub struct CLI {
-    /*
-        /// Name of the person to greet
-        // #[clap(short, long, value_parser)]
-        // pub name: String,
-
-        /// Number of times to greet
-        // #[clap(short, long, value_parser, default_value_t = 1)]
-        // pub count: u8,
-    */
-}
-
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
-    }
-}
+pub mod cli;
+pub mod content_source;
+pub mod driver;
+pub mod logger;
+pub mod querys;
+pub mod text_padding;
